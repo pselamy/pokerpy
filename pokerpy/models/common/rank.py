961bd7cf2b1642440a7204252fe49cebff72c9df
Patrick Selamy <patrick.selamy@cleardata.com>
2017-06-05 18:19:28 -0500

    add `Rank()`

---
+++ pokerpy/models/common/rank.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Rank(Enum):
+    ACE = 14
+    KING = 13
+    QUEEN = 12
+    JACK = 11
+    TEN = 10
+    NINE = 9
+    EIGHT = 8
+    SEVEN = 7
+    SIX = 6
+    FIVE = 5
+    FOUR = 4
+    THREE = 3
+    TWO = 2
