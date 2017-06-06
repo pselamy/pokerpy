42ecfa745d397ddab3ef723a5235af71d18e909d
Patrick Selamy <patrick.selamy@cleardata.com>
2017-06-05 20:12:53 -0500

    require cards to construct deck

--- pokerpy/models/common/deck.py
+++ pokerpy/models/common/deck.py
@@ -6,5 +6,3 @@ from pokerpy.models.common.suit import Suit
 
 
 class Deck(object):
-    def __init__(self, cards=[Card(rank=rank, suit=suit) for rank, suit in product(Rank, Suit)]):
-        self.cards = cards
